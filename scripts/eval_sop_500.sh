93f18219396dbce02689f51c67e4ce924bf71a01
FUWEN TAN <fuwen.tan@gmail.com>
2021-08-31 23:57:18 +0100

    add sop scripts

---
+++ scripts/eval_sop_500.sh
@@ -0,0 +1,4 @@
+python match_sop.py --data_dir $(RRT_SOP)/data \
+    --nn_file $(RRT_SOP)/rrt_sop_caches/rrt_superpoint_sop_nn_inds_test.pkl \
+    --max_keypoints 500
+    --keypoint_threshold 0.005
